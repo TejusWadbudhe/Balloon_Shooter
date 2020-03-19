6c7c63ba4a96cb65eb7da2234ddd70c442b2b284
TejusWadbudhe <tejuswadbudhe06@gmail.com>
2020-03-19 08:56:25 +0530

    First Commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] balloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());   
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(4);
+        for(int i=0; i<3; i++)
+        {
+            Instantiate(balloon[i], spawnPoints[i].position, Quaternion.identity);
+        }
+        StartCoroutine(StartSpawning());
+    }
+  
+}
